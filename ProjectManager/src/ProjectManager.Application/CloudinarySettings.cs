2d954f699ab6bcceb76c280e0872050eadecd04d
Faelkk <Achten.rafael16@gmail.com>
2025-09-10 20:30:52 -0300

    feat: add CloudinaryService and CloudinarySettings

---
+++ ProjectManager/src/ProjectManager.Application/CloudinarySettings.cs
@@ -0,0 +1,8 @@
+namespace ProjectManager.Application;
+
+public class CloudinarySettings
+{
+    public string CloudName { get; set; } = null!;
+    public string ApiKey { get; set; } = null!;
+    public string Secret { get; set; } = null!;
+}
---
+++ ProjectManager/src/ProjectManager.Application/Services/CloudinaryService.cs
@@ -0,0 +1,54 @@
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Options;
+using ProjectManager.Application.DTOS;
+using ProjectManager.Application.Interfaces;
+
+
+namespace ProjectManager.Application.Services;
+
+public class CloudinaryService : ICloudinaryService
+{
+    private readonly Cloudinary _cloudinary;
+
+    public CloudinaryService(IOptions<CloudinarySettings> options)
+    {
+        var settings = options.Value;
+        var account = new Account(settings.CloudName, settings.ApiKey, settings.Secret);
+        _cloudinary = new Cloudinary(account);
+    }
+
+    public async Task<CloudinaryUploadResult> UploadImage(string fileName, Stream fileStream)
+    {
+        var uploadParams = new ImageUploadParams
+        {
+            File = new FileDescription(fileName, fileStream),
+            UseFilename = true,
+            UniqueFilename = false,
+            Overwrite = true,
+        };
+
+        var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+        if (uploadResult.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            return new CloudinaryUploadResult
+            {
+                Url = uploadResult.SecureUrl.ToString(),
+                PublicId = uploadResult.PublicId
+            };
+        }
+        else
+        {
+            throw new Exception($"Falha no upload do arquivo: {uploadResult.Error?.Message}");
+        }
+    }
+
+    public async Task<bool> DeleteFileAsync(string publicId)
+    {
+        var deleteParams = new DeletionParams(publicId);
+        var deleteResult = await _cloudinary.DestroyAsync(deleteParams);
+
+        return deleteResult.Result == "ok";
+    }
+}
